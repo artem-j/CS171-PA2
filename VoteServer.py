57dc2d9780f442e82a17b05383566e90a3e76c6c
Artem Jivotovski <artem.jivotov@gmail.com>
2018-04-26 01:43:32 -0700

    Cleaned up messy code, added print lock
    Cleaned up some extraneous and (slightly) messy code. Added a print lock to ensure that user gets to enter next server's IP and port before being prompted to vote. Added comments outlining skeleton of replicated dictionary messaging.

--- VoteServer.py
+++ VoteServer.py
@@ -1,5 +1,4 @@
 import socket, threading, time
-import sys
 from _thread import *
 
 log = []
@@ -7,24 +6,34 @@ tt = [[0,0,0],[0,0,0],[0,0,0]]
 
 votes = {"A": 0, "B": 0}
 voteLock = threading.Lock()
+printLock = threading.Lock()
 
 def sendUpdate():
+
     global log
     global tt
     global serverID
+
     sendServerID = str((serverID + 1) % 3)
     initMessage = "serverNum:" + sendServerID
 
     sendIP = input("Please enter the IP address of server " + sendServerID + ": ")
     sendPort = int(input("Please enter the port of server " + sendServerID + ": "))
+    printLock.release()
 
     sendSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sendSocket.connect((sendIP, sendPort))
     sendSocket.sendall(initMessage.encode())
 
+    #while True:
+        ##Create a message containing log and timetable
+        #time.sleep(3)
+        #sendSocket.sendall(message.encode())
+
 def recvUpdate():
 
     global serverID
+
     serverListener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serverListener.bind(("127.0.0.1", (8000 + serverID)))
     serverListener.listen(1)
@@ -33,39 +42,49 @@ def recvUpdate():
     data = connection.recv(1024).decode()
 
     recvID = data.split(":")[1]
-    assert serverID == recvID
+    # assert recvID == serverID
 
-    print(data)
+    #while True:
+        #data = recv(1024).decode()
+        #if not data:
+            #break
+        ##Extract log and timetable from data
+        #voteLock.acquire()
+        ##Update log, dictionary, and time table
+        #voteLock.release()
 
 def Main():
     global log
     global tt
     global serverID
 
-    serverID = 3
-    while (serverID > 2):
-        serverID = int(input("Please enter server ID: "))
-        if (serverID > 2):
-            print("Error: Only 3 servers permitted. Server ID must be 0, 1, or 2.")
+    serverID = int(input("Please enter server ID: "))
 
+    while (serverID > 2):
+        serverID = int(input("Error: Only 3 servers permitted. Please enter a valid server ID (0, 1, or 2): "))
+
+    printLock.acquire()
     start_new_thread(recvUpdate, ())
     start_new_thread(sendUpdate, ())
 
-    time.sleep(10) #Fix later
-    vote = ""
+    printLock.acquire()
     while True:
         vote = input("Please enter A to vote for Alice, and B to vote for Bob (or Q to to quit): ")
 
-        if vote == "Q":
-            break
-        elif vote == "A" or vote == "B":
+        if vote == "A" or vote == "B":
             voteLock.acquire()
+
             votes[vote] += 1
             vote = "Vote:" + vote
             log.append(vote)
+            tt[serverID][serverID] += 1
+
             voteLock.release()
+        elif vote == "Q":
+            break
         else:
             print("Error: Invalid input.")
+    printLock.release()
 
 
 if __name__ == "__main__":
